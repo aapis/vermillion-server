b146faf953bc788c1c74d705fd83673c7976048c
Ryan Priebe <rpriebe@me.com>
2016-04-22 13:36:31 -0600

    implemented a more generic version of hash_equals to prevent timing attacks

--- src/CliBundle/Controller/RequestAuthorization.php
+++ src/CliBundle/Controller/RequestAuthorization.php
@@ -27,17 +27,28 @@ class RequestAuthorization {
     private function _validateKey($secret){
         $key = $this->_request->headers->get('x-vermillion-key');
 
-        if(version_compare(PHP_VERSION, '5.6.0') >= 0){
-            $this->success = hash_equals($key, sha1($secret));
-        }else {
-            $this->success = ($key === sha1($secret));
-        }
+        $this->success = $this->_hash_equals($key, sha1($secret));
 
         if(!$this->success){
             $this->message = "Invalid authentication key";
         }
 
         return $this;
+    }
+
+    /**
+     * Cross-PHP version of hash_equals for versions <= 5.6
+     * @from http://php.net/manual/en/function.hash-equals.php#115635
+     */
+    private function _hash_equals($str1, $str2){
+        if(strlen($str1) != strlen($str2)) {
+          return false;
+        } else {
+          $res = $str1 ^ $str2;
+          $ret = 0;
+          for($i = strlen($res) - 1; $i >= 0; $i--) $ret |= ord($res[$i]);
+          return !$ret;
+        }
     }
 }
 
