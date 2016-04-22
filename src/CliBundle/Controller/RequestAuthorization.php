501a7f6e7e6dda56043464162c9132e5b6490296
Ryan Priebe <rpriebe@me.com>
2016-04-22 16:43:57 -0600

    actually use hash_equals when PHP > 5.6

--- src/CliBundle/Controller/RequestAuthorization.php
+++ src/CliBundle/Controller/RequestAuthorization.php
@@ -41,13 +41,17 @@ class RequestAuthorization {
      * @from http://php.net/manual/en/function.hash-equals.php#115635
      */
     private function _hash_equals($str1, $str2){
-        if(strlen($str1) != strlen($str2)) {
-          return false;
-        } else {
-          $res = $str1 ^ $str2;
-          $ret = 0;
-          for($i = strlen($res) - 1; $i >= 0; $i--) $ret |= ord($res[$i]);
-          return !$ret;
+        if(function_exists('hash_equals')){
+            return hash_equals($str1, $str2);
+        }else {
+            if(strlen($str1) != strlen($str2)) {
+              return false;
+            } else {
+              $res = $str1 ^ $str2;
+              $ret = 0;
+              for($i = strlen($res) - 1; $i >= 0; $i--) $ret |= ord($res[$i]);
+              return !$ret;
+            }
         }
     }
 }
